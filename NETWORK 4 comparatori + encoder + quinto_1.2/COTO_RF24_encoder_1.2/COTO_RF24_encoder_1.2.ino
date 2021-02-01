046795fa6fcfe4aac1b04a889beead36419b7fd5
Tdmax77 <47336361+Tdmax77@users.noreply.github.com>
2021-02-01 16:15:59 +0100

    Tutto funzionante
    Cren (encoder)
    funziona sia con il master che con il Coto (Display).

--- NETWORK 4 comparatori + encoder + quinto_1.2/COTO_RF24_encoder_1.2/COTO_RF24_encoder_1.2.ino
+++ NETWORK 4 comparatori + encoder + quinto_1.2/COTO_RF24_encoder_1.2/COTO_RF24_encoder_1.2.ino
@@ -1,35 +1,21 @@
-/*This is the code for the master in our project " WARTSILA WIRELESS COMPARATORS and ENCODER (Kren) READING SYSTEM "
+/*This is the code for the COTO (Display) in our project " WARTSILA WIRELESS COMPARATORS and ENCODER (Kren) READING SYSTEM "
 
-   The target is to read 4 comparators and an encoder  that continuously send data to a central unit.
-
+   The target is to read the encoder  that continuously send data to a central unit.
 
    WARNING: LET'S START FROM THE ASSUMPTION THAT THE VALUES READED FROM THE NODES (COMPARATORS) ARE INCLUDED BETWEEN 0 AND 1360
    OTHERWISE WE COULD HAVE ABNORMAL BEHAVIOR
 
   How it works:
 
-   Every comparator has a transmitter and continuosly sends data to the network.
-   Every node ( comparator + transmitter) sends  a payload with 3 informations:
+   Encoder has a transmitter and continuosly sends data to the network with 3 informations:
    header.from_node  is its adress
    payload.num_sent  is the value readed from comparator
    payload.control   is a number that increases on every cycle. We need it for to decide if the the node is active or not. If it fails, control will not increase anymore.
+   payload.VO that is the offset value (readed from index on Flywheel)
 
-   The central unit (for which this code is written), on every cycle read a packet from the network and analyzes it:
-   It reads who is the sender and the data, then if they are valid, stores them into data array.
-   The reading from the network is not sequential, it is random so i need to create the array for to assure that it read all the nodes of my net (all the comparators and the encoder)
-   On every cycle i incrase the counter of all nodes excepted the one i read. If a node is down his counter will increase untill it will reach the maximum allowed value (Nnosignal)
-   In this case the the data sended to raspberry will be 8888.
-
-   Example: if i read comp1, comp2, comp4 correctly but comp3 is power off i will read something like:
-   0000,0100,8888,0099
-   0000,0101,8888,0099
-
-   In this 2 lines i see that comp 1 is not changed , comp2 is increased by 1 cents, comp3 is not reachable, comp4 is not changed.
-   On every cycle the code sends all the data to the raspberry which is demanded to control graphic interface and data storage.
-
-   The encoder (kren) is the node_5, it requires a different approach because its value needs to be aligned with flywheel index using an OFFSET Value (VO).
+   The encoder (kren) is the node_5, it needs to be aligned with flywheel index using an OFFSET Value (VO) because we can't start everytime from 0°.
    Kren asks for an Offset value when it starts (OffsetReq = 1), Offset value is setted to 9999 (VO = 9999).
-   So Master wait for this value from user. Then it stores the data in val, send it to encoder and esc from Offset task.
+   So Display(COTO) wait for this value from user. Then it stores the data in var, send it to encoder and esc from Offset task.
    Because of a "strange problem" it asks 4-5 times the offset value (probably a timing problem) so i introduced a condition y.
    y is setted to 1 all the time,this is a condition for enter into Offset task, after the task y becomes 0 for only 0,1 sec so the program can't enter again in Offset task.
    After 0,1 sec y goes to 1 so, if encoder is closed and reopen, master could enter into Offset task again.
@@ -38,7 +24,7 @@
    Release 1.1
 */
 
-
+#define DEBUG
 
 #include <RF24Network.h>
 #include <RF24.h>
@@ -65,19 +51,17 @@ struct payload_t {              // Structure of  payload received from slaves
 };
 payload_t payload;
 
-long valoreangolocorrettoPrev;
-int mostraangolo = 0;
-float angprint;
-float angstamp;
+long valoreangolocorrettoPrev;  // needed for data verify
+int mostraangolo = 0;           // needed for redisplay after lost conn
+float angprint;                 // needed for to convert long data into float (we receive a long from the net)
+float angstamp;                 // needed for to convert long data into float (we receive a long from the net)
 
-int dummy_val (8888);     // define a value that will be printed if COMP fails
-payload_t data;    // create an array data
-int counter ;      // Array defined for validation of data (ses main loop)
-int Nnosignal (30);       // number of acceptable failed reads
+payload_t data;                 // create to arrays payload_t data
+int counter ;                   // Array defined for validation of data (ses main loop)
+int Nnosignal (30);             // number of acceptable failed reads
 
-//int val = 0;              // This is the value readed on the index of flywheel by user and insered by the OFFSET Request task
-unsigned long tempo;      // need for the y counter
-int y = 1;                // need for block OFFSET Request loop
+unsigned long tempo;            // need for the y counter
+int y = 1;                      // need for block OFFSET Request loop
 
 /*Variabili Offset servono ad inserire il valore usando i 3 pulsanti*/
 const int buttonOkPin = 4;
@@ -104,7 +88,7 @@ int readingUp = 0;  //Lettura ingresso digitale del pulsante di UP
 int readingDown = 0;  //Lettura ingresso digitale del pulsante di Down
 
 /*Variabili Offset servono ad inserire il valore usando i 3 pulsanti*/
-//int counter;
+
 int counterprec;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -113,7 +97,7 @@ int counterprec;
 
 void setup(void)
 {
-  Serial.begin(38400);                                       // initialize serial for to send data to raspberry
+
   SPI.begin();                                               // initialize SPI
   radio.begin();                                             // initialize radio module
   network.begin(/*channel*/ 90, /*node address*/ this_node); // start network
@@ -127,11 +111,11 @@ void setup(void)
   /* parte display i2c */
   lcd.init();                      // initialize the lcd
   lcd.backlight();
-  /*  lcd.setCursor(0, 0);
-    lcd.print("Crankshaft");
-    lcd.setCursor(0, 1);
-    lcd.print("Wireless reader");
-    delay(3000);*/
+  lcd.setCursor(0, 0);
+  lcd.print("Crankshaft");
+  lcd.setCursor(0, 1);
+  lcd.print("Wireless reader V1.2");
+  delay(3000);
   lcd.clear();
   /* parte display i2c */
 
@@ -160,32 +144,31 @@ void loop(void) {
 
 
   network.update();           // looking for news on the network
-  ser();
+  delay(5);                   // needed at least 1 ms of delay or display stay in NOT CONN state (i don't know why ...
+  while ( network.available() ) {                             // if network is up  ... I need at least one slave active for to keep active the network, otherwise no network will be available
 
-  while ( network.available() ) {                            // if network is up  ... I need at least one slave active for to keep active the network, otherwise no network will be available
-  
-    if (mostraangolo == 1) {
-        lcd.setCursor(0, 0);
-        lcd.print("    Angolo:     ");
-        lcd.setCursor(10, 1);
-        lcd.print("Gradi");
-        lcd.setCursor(0, 1);
-        lcd.print("          ");
-        lcd.setCursor(1, 1);
-        lcd.print(angstamp);
-        mostraangolo = 0;
-      }
-    
+    if (mostraangolo == 1) {                                  // after a lost connection it need to refresh display also if value is not changed
+      lcd.setCursor(0, 0);
+      lcd.print("    Angolo:     ");
+      lcd.setCursor(10, 1);
+      lcd.print("Gradi");
+      lcd.setCursor(0, 1);
+      lcd.print("          ");
+      lcd.setCursor(1, 1);
+      lcd.print(angstamp);
+      mostraangolo = 0;                                     // reset value
+    }
+
     int cnt (0);                                             // reset counter
     RF24NetworkHeader header;                                // declare the address of this node: every packet sent from slaves to header will be readed from this node (00, the master)
     network.read(header, &payload, sizeof(payload));         // read the packet netwotk sent for this node
     size_t node_id = header.from_node;                       // create a variable node_id that stores the source of the packet ( who is sending it to me? COMP1 with 01 , COMP2 with 02 and so on..
 
     if ((payload.OffsetReq == 1) && (payload.VO = 9999) && (y == 1)) {     // if Offset is request i start the Offset task
-      testo_richiesta_inserimento_offset();
-      while (digitalRead(buttonOkPin) == LOW )
+      testo_richiesta_inserimento_offset();                                // ask for offset
+      while (digitalRead(buttonOkPin) == LOW )                             // insert value and wait for OK button pressed
       {
-        PROCEDURA_OFFSET();
+        PROCEDURA_OFFSET();                                                // Offset procedure
       }
       payload.OffsetReq = 0;                                                // reset the Offset request
       payload.VO = var;                                                     // assign offset value to payload
@@ -198,19 +181,19 @@ void loop(void) {
   }
 
   if (data.control != payload.control) {      // if control readed from network is different from control stored i assume that the packet is new, so the slave is alive and the data is valid
-    data.num_sent = payload.num_sent;         // so i store readed values in data.num_sent ( this is comparator's readed value from slave)
-    data.control = payload.control;           // update data.control to new value received  (this is the counter sent from slave that increase on every sending)
+    data.num_sent = payload.num_sent;         // so i store readed values in data.num_sent ( this is encoder value)
+    data.control = payload.control;           // update data.control to new value received  (this is the counter sent from encoder that increase on every sending)
     counter = 0;                              // reset couter of the node i readed in this cycle.
   } else {
-    counter = counter += 1;                                                      // A valid data is included in a range from 0 to 1360 that is the values readed from Mitutoyo comparator (0-1360 cent)
+    counter = counter += 1;                   // icrease counter
   }
 
-  if (counter >= Nnosignal) {
-    display_no_conn();
+  if (counter >= Nnosignal) {                 // if counter reach Nnosignal
+    display_no_conn();                        // display no conn for 2 seconds
     delay(2000);
-    mostraangolo = 1;
+    mostraangolo = 1;                         // but variable to 1 for refresh display next cycle
   } else {
-    display_angolo();
+    display_angolo();                         // if all ok display value
   }
 }
 
@@ -245,11 +228,6 @@ void readButtonState() {
 
 void PROCEDURA_OFFSET() // mi restituisce un valore var che ho inserito come offset
 {
-#ifdef DEBUG
-  Serial.println("");
-  Serial.println("SONO NELLA PROCEDURA OFFSET()");
-  Serial.println("");
-#endif
   readButtonState();  //Lettura stato buttons con controllo antirimbalzo
 
   if (buttonUpState == HIGH || buttonDownState == HIGH) {
@@ -315,11 +293,11 @@ void testo_richiesta_inserimento_offset() {
 }
 
 void display_angolo() {
-  if (payload.num_sent != valoreangolocorrettoPrev)//ridisegno il display unicamente se il dato dell'angolo cambia (evito flashamenti )
+  if (payload.num_sent != valoreangolocorrettoPrev)     // refresh display only if angle changes
   {
-    valoreangolocorrettoPrev = payload.num_sent;
-     angprint = payload.num_sent;
-     angstamp = angprint/100;
+    valoreangolocorrettoPrev = payload.num_sent;        // read angle value
+    angprint = payload.num_sent;                       //convert from long to float
+    angstamp = angprint / 100;                         // add the comma
     lcd.setCursor(0, 0);
     lcd.print("    Angolo:     ");
     lcd.setCursor(10, 1);
@@ -329,18 +307,4 @@ void display_angolo() {
     lcd.setCursor(1, 1);
     lcd.print(angstamp);
   }
-}
-
-void ser() {
-  Serial.print("num_sent ");
-  Serial.print(payload.num_sent);
-  Serial.print(", control ");
-  Serial.print(payload.control);
-  Serial.print(", OffsetReq ");
-  Serial.print(payload.OffsetReq);
-  Serial.print("     VO ");
-  Serial.print(payload.VO);
-  Serial.print("Counter: ");
-  Serial.print(counter);
-  Serial.println();
 }
